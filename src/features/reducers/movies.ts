746f5c6a9a0af9dd774c87be2e2d45d590215e57
Iryna Rozmaita <iriszechka@ukr.net>
2024-04-04 13:59:40 +0300

    + react router (old), redux store, MovieCard

---
+++ src/features/reducers/movies.ts
@@ -0,0 +1,27 @@
+import { Action, Reducer } from "redux";
+
+export interface Movie {
+    id: number;
+    title: string;
+    popularity: number;
+    overview: string;
+}
+
+interface MovieState{
+    top: Movie[]
+}
+
+const initialState: MovieState = {
+    top: [
+        { id: 1, title: "Inception", popularity: 98, overview: "Dreams..." },
+        { id: 2, title: "The Gotfather", popularity: 97, overview: "Gotfather..." },
+        { id: 3, title: "The Dark Knight", popularity: 96.5, overview: "Batman..." },
+        { id: 4, title: "The Gotfather Part II", popularity: 96, overview: "Part II..." },
+    ]
+};
+
+const moviesReducer: Reducer<MovieState, Action> = (state, action) => {
+    return initialState;
+}
+
+export default moviesReducer;
